5eac8089cd25e127b82ccf7d7e37c5cb1b3e9e19
dbswl31024 <dbswl31024@GZ-dbswl3102001.GFZ.CORP>
2021-08-19 13:13:33 +0900

    [add] post like, unlike에 따른 result csv file에 저장 및 추천 알고리즘 실행 추가

--- golftok/src/main/java/com/golfzon/golftok/controller/LikeController.java
+++ golftok/src/main/java/com/golfzon/golftok/controller/LikeController.java
@@ -1,8 +1,13 @@
 package com.golfzon.golftok.controller;
 
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -21,6 +26,8 @@ import com.golfzon.golftok.service.CommentService;
 import com.golfzon.golftok.service.LikeService;
 import com.golfzon.golftok.service.PostService;
 import com.golfzon.golftok.service.UsersService;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 
 @RestController
 @RequestMapping("like")
@@ -56,6 +63,10 @@ public class LikeController {
 		if (likeService.insertPostLike(likeMap) == 0 || likeService.increasePostLikeCount(postId)==0) {
 			response.sendError(HttpServletResponse.SC_NOT_FOUND);
 		}
+		
+		// 추천 서비스 실행을 위해, 누른 좋아요 csv 파일에 반영
+		String filePath = "C://Users//owner//PycharmProjects//golftok//like_data.csv";
+		readDataFromCsv(filePath,Integer.toString(userId),Integer.toString(postId),true);
 	}
 	
 	// 게시물 좋아요 취소
@@ -74,6 +85,10 @@ public class LikeController {
 		if (likeService.deletePostLike(likeMap) == 0 || likeService.decresePostLikeCount(postId) == 0) {
 			response.sendError(HttpServletResponse.SC_FORBIDDEN);
 		}
+		
+		// 추천 서비스 실행을 위해, 누른 좋아요 csv 파일에 반영
+		String filePath = "C://Users//owner//PycharmProjects//golftok//like_data.csv";
+		readDataFromCsv(filePath,Integer.toString(userId),Integer.toString(postId),false);
 	}
 	
 	// 댓글 좋아요
@@ -95,7 +110,7 @@ public class LikeController {
 		}
 	}
 	
-	// 좋아요 취소
+	// 댓글 좋아요 취소
 	@DeleteMapping("comment")
 	@ResponseStatus(code = HttpStatus.OK)
 	public void unlikeComment(@RequestParam(value = "commentId") int commentId, HttpServletResponse response,
@@ -167,5 +182,63 @@ public class LikeController {
 		map.put("likeCount", commentService.getCommentLikeCount(commentId));
 		
 		return map;
+	}
+
+	// 좋아요가 변경 될 때마다 csv 파일 수정
+	public static void readDataFromCsv(String filePath,String userId, String postId, boolean flag) throws IOException {
+		// 좋아요 시, csv 파일에 데이터 추가
+		if(flag==true) {
+			CSVWriter csvWriter = new CSVWriter(new FileWriter(filePath,true));
+			String[] likeData = new String[] {userId,postId,"1"};
+			csvWriter.writeNext(likeData);
+			csvWriter.close();
+			
+		}else{// 좋아요 취소 시, csv 파일 데이터 삭제
+			CSVReader reader = new CSVReader(new FileReader(filePath));
+			String[] nextLine;
+			ArrayList<String> likeDateList = new ArrayList<String>();
+	
+			int csvRow=0;
+			int breakFlag=0;
+			while ((nextLine = reader.readNext()) != null) { 
+				for (int i = 0; i < nextLine.length; i++) {
+					likeDateList.add(nextLine[i]);
+				}
+				
+				int idx=0;
+				boolean b=true;
+				for(String s : likeDateList) {
+					if(idx==0) {
+						if (!s.equals(userId)) {
+							b=false;
+							break;
+						}
+					}
+					if(idx==1) {
+						if(!s.equals(postId)) {
+							b=false;
+							break;
+						}
+					}
+					idx+=1;
+					if(idx>2) break;
+				}
+				
+				// 조건에 맞는 행을 찾으면 csv 파일에서 행 삭제 후, 다시 쓰기
+				if(b) {
+					reader = new CSVReader(new FileReader(filePath));
+					List<String[]> allElements = reader.readAll();
+					allElements.remove(csvRow);
+					CSVWriter csvWriter = new CSVWriter(new FileWriter(filePath));
+					csvWriter.writeAll(allElements);
+					csvWriter.close();
+					breakFlag=1;
+				}
+				
+				likeDateList.clear();
+				if(breakFlag==1) break;
+				csvRow+=1;
+			}
+		}
 	}
 }
